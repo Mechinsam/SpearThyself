89c6e2ab2747b2cee0ff0502d0402f798be59cc5
Mechinsam <78374707+Mechinsam@users.noreply.github.com>
2024-11-05 17:54:39 +0000

    [NOT WORKING] checking if a spear is being held by slugcat

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spear Thyself (Self-Spearing Rain World Mechanic)
+-----
+Source code for 'Spear Thyself' RW mod. Basically you stab yourself with a spear. lol!!!!!!!
+
+Relies on .NET 4.8 and C# 11 (if compiling)
+
+ummmm no .csproj or .sln file because i dont know how to distrubute those correctly xd
---
+++ SpearThyself.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+
+// bypass fields that are set to private
+#pragma warning disable CS0618
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace spearthyself;
+
+[BepInPlugin("mechinsam.spearthyself", "Spear Thyself", "1.0.0")]
+
+
+public partial class SpearThyself : BaseUnityPlugin
+{
+	private void OnEnable()
+	{
+		On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+	}
+
+	private bool IsInit;
+	private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+	{
+		orig(self);
+		try
+		{
+			if (IsInit) return;
+
+			// hooks
+			On.Player.Update += PlayerOnUpdate;
+
+			On.RainWorldGame.ShutDownProcess += RainWorldGameOnShutDownProcess;
+			On.GameSession.ctor += GameSessionOnctor;
+			
+			IsInit = true;
+		}
+		catch (Exception ex)
+		{
+			Logger.LogError(ex);
+			throw;
+		}
+	}
+	
+	// free variables and methods from memory when shutting down rain world
+	private void RainWorldGameOnShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
+	{
+		orig(self);
+		ClearMemory();
+	}
+	private void GameSessionOnctor(On.GameSession.orig_ctor orig, GameSession self, RainWorldGame game)
+	{
+		orig(self, game);
+		ClearMemory();
+	}
+
+	#region Helper Methods
+
+	private void ClearMemory()
+	{
+		//If you have any collections (lists, dictionaries, etc.)
+		//Clear them here to prevent a memory leak
+		//YourList.Clear();
+	}
+
+	#endregion
+}
