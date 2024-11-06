116e8bf848e8385025dce44466b01bdb8ce6526a
Mechinsam <78374707+Mechinsam@users.noreply.github.com>
2024-11-06 21:32:35 +0000

    [WORKING!!!!!!!!] made check if player is holding spear code
    and also remove the spear if they are LOL!!!!!!!!!!!
    
    deleted mod folder (ill add them later when im finished with this)
    
    also fixed code yippe!!!!!

--- Player.cs
+++ Player.cs
@@ -10,44 +10,45 @@ public partial class SpearThyself
 	{
 		orig(slugcat, eu); //Always call original code, either before or after your code, depending on what you need to achieve
 
-
 		if (Input.GetKeyDown(KeyCode.I))
-			isHoldingSpear = CheckForSpear(slugcat);
+		{
+			isHoldingSpear = DestroySpear(slugcat);
 
-		if (isHoldingSpear)
-		{
-			slugcat.Regurgitate();
-		} else
-		{
-			slugcat.Jump();
-		}
-		/*if (Input.GetKeyDown(KeyCode.I))
-		{
-			if (slugcat.grasps[1] is Spear)
+			if (isHoldingSpear)
 			{
-				slugcat.Regurgitate();
-			} else
+				slugcat.Die();
+				isHoldingSpear = false;
+			} else // here for debugging purposes
 			{
 				slugcat.Jump();
 			}
-		}*/
-
-		if (Input.GetKeyDown(KeyCode.K))
-			slugcat.Regurgitate();
+		}
 	}
 
-	/*private bool CheckForSpear(Player slugcat)
+	private bool DestroySpear(Player slugcat)
 	{
+		if (slugcat.grasps == null)
+			return false;
+
+
 		foreach (var holding in slugcat.grasps)
 		{
-			if (holding is Spear)
+			// if slot is not empty and is holding a spear
+			if (holding != null && holding.grabbed is Spear)
+			{
+				holding.grabbed.Destroy(); // GET THAT SHIT OUTTA HERE!!!!!!!!
 				return true;
+			}
 		}
 		// if not already returned a value, return false
 		return false;
-	}*/
-	private bool CheckForSpear(Player slugcat)
-	{
-		return false;
 	}
+
+	/*private void SpawnSpearInSlugcat(Player slugcat)
+	{
+		World world = slugcat.room.world;
+		WorldCoordinate initalSpawnPos = new WorldCoordinate(slugcat.room.index, slugcat.bodyChunks[0].pos);
+
+		AbstractPhysicalObject abtractObject = new AbstractPhysicalObject(slugcat.room.world, AbstractPhysicalObject.AbstractObjectType.Spear, null, )
+	}*/
 }
